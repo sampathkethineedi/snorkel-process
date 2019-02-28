95144be3b00c239aaff7baf163be6176fc9761d5
sampathkethineedi <sampath.kethineedi9@gmail.com>
2019-02-28 13:14:54 +0530

    added label table create and push

--- db_process.py
+++ db_process.py
@@ -1,14 +1,17 @@
 import sqlite3
 import os.path
+from snorkel import SnorkelSession
 
 
 class DataBase(object):
-    def __init__(self, name):
+    def __init__(self, name, version=0.1):
         """
         Create DB connection
         :param name: Database name
         """
+        self.session = SnorkelSession()
         self.name = name
+        self.version = version
         if os.path.isfile(self.name + '.db'):
             os.remove(self.name + '.db')
         self.conn = sqlite3.connect(self.name + '.db')
@@ -19,20 +22,31 @@ class DataBase(object):
         :return: None
         """
         c = self.conn.cursor()
+
         c.execute('CREATE TABLE document ('
-                  'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                  'id INTEGER PRIMARY KEY,'
                   'text TEXT NOT NULL,'
                   'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
                   'version INTEGER)')
 
         c.execute('CREATE TABLE sentence ('
-                  'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                  'id INTEGER PRIMARY KEY,'
                   'text TEXT NOT NULL,'
-                  'doc_id INTEGER, '
+                  'document_id INTEGER, '
                   'position INTEGER,'
                   'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
                   'version INTEGER,'
-                  'FOREIGN KEY(doc_id) REFERENCES document(id))')
+                  'FOREIGN KEY(document_id) REFERENCES document(id))')
+
+        c.execute('CREATE TABLE label ('
+                  'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                  'sentence_id INTEGER,'
+                  'document_id INTEGER,'
+                  'probability FLOAT,'
+                  'lf_func INTEGER,'
+                  'timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,'
+                  'FOREIGN KEY(sentence_id) REFERENCES sentence(id),'
+                  'FOREIGN KEY(document_id) REFERENCES sentence(document_id))')
 
         self.conn.commit()
         c.close()
@@ -45,22 +59,54 @@ class DataBase(object):
         """
         c = self.conn.cursor()
         for item in sentence_data:
-            c.execute('insert into sentence(doc_id,position,text,version) values (?,?,?,?)', (*item, 1))
+            c.execute('insert into sentence(id, document_id,position,text,version) '
+                      'values (?,?,?,?,?)', (*item, self.version))
+        self.conn.commit()
+        c.close()
+
+    def push_label_table(self, label_data):
+        """
+        Insert Snorkel sentence data into DB
+        :param label_data: Snorkel label data
+        :return: None
+        """
+        c = self.conn.cursor()
+        for item in label_data:
+            c.execute('insert into label(document_id,sentence_id,lf_func,probability) '
+                      'values (?,?,?,?)', item)
         self.conn.commit()
         c.close()
 
 
-def get_snorkelDB_data():
+def get_sentence_data():
     """
-    Get data from snorkel
+    Obtain sentence data from DB
     :return: sentence_data
     """
     conn_snorkel = sqlite3.connect('snorkel.db')
     c = conn_snorkel.cursor()
-    c.execute('SELECT document_id,position,text FROM sentence')
+    c.execute('SELECT id, document_id,position,text '
+              'FROM sentence')
     sentence_data = c.fetchall()
     c.close()
     return sentence_data
+
+
+def get_label_data():
+    """
+    Perform product and obtain mariginals data
+    :return: label_data
+    """
+    conn_snorkel = sqlite3.connect('snorkel.db')
+    c = conn_snorkel.cursor()
+    c.execute('SELECT document_id,sentence.id,value,probability '
+              'FROM text,sentence,marginal,span '
+              'WHERE candidate_id = text.id '
+              'AND text.text_id = span.id '
+              'AND span.sentence_id = sentence.id')
+    label_data = c.fetchall()
+    c.close()
+    return label_data
 
 
 if __name__ == "__main__":
@@ -71,5 +117,7 @@ if __name__ == "__main__":
 
     test_db = DataBase(args.name)
     test_db.create_tables()
-    sent_data = get_snorkelDB_data()
+    sent_data = get_sentence_data()
+    lab_data = get_label_data()
     test_db.push_sentence_table(sent_data)
+    test_db.push_label_table(lab_data)
